e189f26f06b0e7c970c991c3fd9ef3ff25d23ebd
ritik723 <ritiksingh7239@gmail.com>
2021-05-22 19:29:39 +0530

    result displayed by calling the api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,59 @@
-import React, {useState} from 'react'
-import MainPageLayout from '../components/MainPageLayout'
+import React, { useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
-    const [input, setInput] = useState('');
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
-    const onSearch = () =>{
-        // http://api.tvmaze.com/singlesearch/shows?q=girls
+  const onSearch = () => {
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
+  };
 
-        fetch(`https://api.tvmaze.com/singlesearch/shows?q=${input}`).then(r => r.json()).then(result => {
-            console.log(result);
-        });
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
 
-    };
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
 
-    const onInputChange = ev => {
-        setInput(ev.target.value);
-        console.log(ev.target.value);
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
     }
 
-    const onKeyDown = ev => { 
-        if(ev.keyCode === 13) {
-            onSearch()
-        }
-
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
     }
 
-    
-    return (
-        <MainPageLayout>
-             <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
-             <button type="button" onClick={onSearch}>Search</button>
-            
-        </MainPageLayout>
-    )
-}
+    return null;
+  };
 
-export default Home
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResults()}
+    </MainPageLayout>
+  );
+};
+
+export default Home;
