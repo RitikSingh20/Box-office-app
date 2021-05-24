5fd739ace987fb9762c388ce1d9a7a7d5241bcb6
ritik723 <ritiksingh7239@gmail.com>
2021-05-24 20:41:48 +0530

    added show page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
  
 import Home from './pages/Home'
 import Starred from './pages/Starred'
+import Show from './pages/Show'
 
 function App() {
   return (
@@ -16,6 +17,10 @@ function App() {
 
       <Route exact path="/starred">
        <Starred/>
+      </Route>
+
+      <Route exact path="/show/:id">
+        <Show />
       </Route>
 
       <Route><div>This is 404 page</div></Route>
---
+++ src/pages/Show.js
@@ -0,0 +1,28 @@
+import React, {useEffect,useState}from 'react'
+import {useParams} from 'react-router-dom';
+import { apiGet } from '../misc/config';
+
+
+const Show = () => {
+    const {id} = useParams();
+
+    const [show, setShow] = useState(null);
+
+    useEffect( () => {
+
+        apiGet(`/shows/${id}?embed[]=season&embed[]=cast`).then(results=> {
+         setShow(results); 
+        })
+        
+    }, [id])
+
+    console.log('show',show);
+    
+    return (
+        <div>
+            this is show page
+        </div>
+    )
+}
+
+export default Show
